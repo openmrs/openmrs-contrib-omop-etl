82942fcb2c1dae78b7908561193b0ba47181420b
jayasanka-sack <33048395+jayasanka-sack@users.noreply.github.com>
2025-04-02 15:20:40 +0530

    Generate observation period table

---
+++ models/onservation_period.sql
@@ -0,0 +1,26 @@
+MODEL (
+  name omop_db.OBSERVATION_PERIOD,
+  kind FULL
+);
+
+
+SELECT
+    ROW_NUMBER() OVER (ORDER BY MIN(v.date_started)) AS observation_period_id,
+        v.patient_id AS person_id,
+    DATE(MIN(v.date_started)) AS observation_period_start_date,
+    DATE(
+    GREATEST(
+    MAX(v.date_stopped),
+    MAX(e.encounter_datetime)
+    )
+    ) AS observation_period_end_date,
+    44814724 AS period_type_concept_id  -- EHR record
+FROM
+    openmrs.visit v
+    LEFT JOIN
+    openmrs.encounter e ON v.visit_id = e.visit_id
+WHERE
+    v.date_started IS NOT NULL
+GROUP BY
+    v.patient_id;
+
